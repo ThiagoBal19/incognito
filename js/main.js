06e682ad904b45af6618adb3b6a77fb0dc52866f
Thiago Balderrama <tbalderrama8@gmail.com>
2022-02-12 00:55:15 -0300

    mejorado a su versión 2.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Efect Slide Down & Up in Banner
+// Swipe up and down effect on the banner
 let show = false;
 $(".btnDescription").click(() => {
     if (show)
@@ -8,87 +8,152 @@ $(".btnDescription").click(() => {
     show = !show;
 })
 
-
-// PRODUCTS
-
-// Orders
-function limpiar() {
-    document.getElementById('finalCompra').reset();
-}
-function limpiarTwo() {
-    document.getElementById('finalCompraDos').reset();
-}
-
-let resultado;
-const Total = function total() {
-    let suitSelected = document.getElementById("suitName")
-    let cost = document.getElementById("costo");
-
-    // Suit 1
-    $(".suitOne").click(() => {
-        suitSelected.innerHTML = "Leggerissimo Blue Suit";
-        cost.innerHTML = 2100;
-    })
-
-    // Suit 2
-    $(".suitTwo").click(() => {
-        suitSelected.innerHTML = "Stretch Mohair Wool Suit"
-        cost.innerHTML = 2620;
-    })
-
-    // Suit 3
-    $(".suitThree").click(() => {
-        suitSelected.innerHTML = "Striped Wool Silk Linen Suit"
-        cost.innerHTML = 3550;
-    })
-
-    // Suit 4
-    $(".suitFour").click(() => {
-        suitSelected.innerHTML = "Natural Stretch Dark Gray Suit"
-        cost.innerHTML = 2450;
-    })
-
-    // Suit 5
-    $(".suitFive").click(() => {
-        suitSelected.innerHTML = "Light Gray Silk Wool Suit"
-        cost.innerHTML = 2820;
-    })
-
-    // Suit 6
-    $(".suitSix").click(() => {
-        suitSelected.innerHTML = "Natural Stretch Dark Suit"
-        cost.innerHTML = 3360;
-    })
-
-    let numero1 = parseInt(document.getElementById("cantidad").value);
-    let numero2 = cost.innerHTML;
-
-    if (!numero1) {
-        return;
-    }
-
-    resultado = numero1 * numero2;
-
-    let precioFinal = document.getElementById("resultado");
-    precioFinal.innerHTML = "$" + resultado;
-}
-
-let finPedido = document.getElementById("resolver");
-let finalizarPedido = document.getElementById("finalCompra");
-finPedido.onclick = 
-    Total,
-    finPedido.click(() => {
-        finalizarPedido.fadeout("fast");
+// Scroll function to the top of the brand logo
+function goTop() {
+    scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
-    
+}
+
+
+// Array of Products
+let sectionProducts = document.getElementById("products");
+let info;
+
+const arrayProducts = [
+    {
+        id: 0,
+        image: "assets/suitBlue.jpg",
+        title: "Leggerissimo Blue Suit",
+        cost: 2100,
+        brand: "REDA",
+    },
+    {
+        id: 1,
+        image: "assets/suitGreen.jpg",
+        title: "Stretch Mohair Wool Suit",
+        cost: 2620,
+        brand: "ZIGNONE",
+    },
+    {
+        id: 2,
+        image: "assets/suitRed.jpg",
+        title: "Striped Wool Silk Linen Suit",
+        cost: 3550,
+        brand: "VITALE BARBERIS CANONICO",
+    },
+    {
+        id: 3,
+        image: "assets/suitDarkGray.jpg",
+        title: "Natural Stretch Dark Gray Suit",
+        cost: 2450,
+        brand: "DRAGO",
+    },
+    {
+        id: 4,
+        image: "assets/suitLightGray.jpg",
+        title: "Light Gray Silk Wool Suit",
+        cost: 2820,
+        brand: "VITALE BARBERIS CANONICO",
+    },
+    {
+        id: 5,
+        image: "assets/suitDark.jpg",
+        title: "Natural Stretch Dark Suit",
+        cost: 3360,
+        brand: "DRAGO",
+    }
+]
+
+// Array on HTML
+info = "<article>";
+    for(const arrayProduct of arrayProducts) {
+        info += "<div  class='descriptionProduct'>" +
+                    "<div>" + 
+                        "<img class='imageProduct' src='" + arrayProduct.image + "'>" +
+                        "<h2>" + arrayProduct.title + "</h2>" +
+                        "<h3>" + "$" + arrayProduct.cost +"</h3>" +
+                        "<h4>" + arrayProduct.brand +"</h4>" +
+                        "<a id='" + arrayProduct.id + "' class='buyBtn' type='button' onClick='fillForm()' href='#endPurchase'>" + "BUY" + "</a>" +
+                    "</div>" +
+                "</div>";
+    }
+info += "</article>";
+
+sectionProducts.innerHTML = info;
+
+// Show array on HTML
+let suitSelected = document.getElementById("suitName")
+let suitCost = document.getElementById("cost");
+
+function fillForm() {
+    $("#0").click(() => {
+        suitSelected.innerHTML = arrayProducts[0].title;
+        suitCost.innerHTML = arrayProducts[0].cost;
+    })
+    $("#1").click(() => {
+        suitSelected.innerHTML = arrayProducts[1].title;
+        suitCost.innerHTML = arrayProducts[1].cost;
+    })
+    $("#2").click(() => {
+        suitSelected.innerHTML = arrayProducts[2].title;
+        suitCost.innerHTML = arrayProducts[2].cost;
+    })
+    $("#3").click(() => {
+        suitSelected.innerHTML = arrayProducts[3].title;
+        suitCost.innerHTML = arrayProducts[3].cost;
+    })
+    $("#4").click(() => {
+        suitSelected.innerHTML = arrayProducts[4].title;
+        suitCost.innerHTML = arrayProducts[4].cost;
+    })
+    $("#5").click(() => {
+        suitSelected.innerHTML = arrayProducts[5].title;
+        suitCost.innerHTML = arrayProducts[5].cost;
+    })
+
+}
+
+// This method is only used to fix an error that appears when we click on the buy anchor.
+setTimeout(fillForm, 0)
+
+let totalResult = 0;
+function calculate() {
+    let quantity = document.getElementById("quantity").value;
+    let suitTotal = document.getElementById("result")
+
+    try {
+        totalResult = quantity * suitCost.innerHTML;
+        suitTotal.innerHTML = "$" + totalResult;
+    } catch (e) {}
+}
+
+let end = document.getElementById("endPurchaseTwo");
+
+// Function Clean inputs
+function clean() {
+    document.getElementById('endPurchase').reset();
+    $("#suitName").empty();
+    $("#cost").empty();
+    $("#result").empty();
+    end.style = "display: none";
+}
+function cleanTwo() {
+    document.getElementById('endPurchaseTwo').reset();
+}
 
 // End of Purchase
-let end = document.getElementById("finalCompraDos");
-    $("#resolver").click(() => {
+function showEnd() {
+    if(totalResult != "") {
         end.style = "display: flex";
-    })
+    } else {
+        alert("Choose your quantity")
+    }
+}
 
 // Modal Edit
+let modalTitle = document.getElementById("endModalTitle");
 let modal = document.getElementById("endModal");
 let endButton = document.getElementById("finishBtn");
 let nameUser = document.getElementById("name");
@@ -104,8 +169,10 @@ $("#finishBtn").click(() => {
     let userDirection = localStorage.getItem("userDirection");
 
     if (userName != "" && userEmail != "" && userDirection != "") {
-        modal.innerHTML = "The order in " + userName + "'s name will be sent to " + userDirection + ". We will send you an email to " + userEmail + " with all the information." + '<br>' + "Total = $" + resultado;
+        modalTitle.innerHTML = "PURCHASE MADE";
+        modal.innerHTML = "The order in " + userName + "'s name will be sent to " + userDirection + ". We will send you an email to " + userEmail + " with all the information." + '<br>' + "TOTAL = $" + totalResult;
     } else {
+        modalTitle.innerHTML = "ERROR";
         modal.innerHTML = "Please sign your personal information";
     }
 })
